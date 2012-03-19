4638b497e41d5fc9c278bef2ef139a25c8eb7f9f
Damon Snyder <drsnyder@gmail.com>
2012-03-18 22:47:08 -0700

    noting the next incremental change

--- src/pidge/data.clj
+++ src/pidge/data.clj
@@ -1,6 +1,7 @@
 (ns pidge.data)
 
 (defprotocol Container
+             ; make score and ident a "Sortable"
              (add  [object score ident])
              (top [object n])
              ; index?
